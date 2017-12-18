3c26309c65289203e78a8adbc2e823de11a8cf7b
Sara Collins <datadivallc@gmail.com>
2017-12-17 21:18:57 -0600

    Adding function modify_object_points function and passing test.

--- lane_finding.py
+++ lane_finding.py
@@ -1,12 +1,29 @@
 import numpy as np
 
-def create_object_points(height, width, depth, type=np.float32):
+
+def create_object_points(height, width, depth, data_type=np.float32):
     """
 
     :param height:
     :param width:
     :param depth:
-    :param type:
+    :param data_type:
     :return:
     """
-    return np.zeros((height*width, depth), type)
+    return np.zeros((height*width, depth), data_type)
+
+
+def modify_object_points(object_points, height, width, slicer,
+                         reshape_0, reshape_1):
+    """
+    Provides modification of initial object points.
+    :param object_points:
+    :param height:
+    :param width:
+    :param slicer:
+    :param reshape_0:
+    :param reshape_1:
+    :return:
+    """
+    object_points[:,:slicer] = np.mgrid[0:width, 0:height].T.reshape(reshape_0, reshape_1)
+    return object_points
--- test_lane_finding.py
+++ test_lane_finding.py
@@ -3,15 +3,29 @@ import numpy as np
 import lane_finding as lf
 
 
-class MyTestCase(unittest.TestCase):
+class TestLaneFinding(unittest.TestCase):
 
     def test_create_object_points(self):
         grid_height = 6
         grid_width = 9
         grid_depth = 3
-        tester_object = np.zeros((grid_height*grid_width,grid_depth), np.float32)
+        tester_object = np.zeros((grid_height*grid_width, grid_depth), np.float32)
         grid_object = lf.create_object_points(grid_height, grid_width, grid_depth)
         self.assertEqual(tester_object.shape, grid_object.shape)
+
+    def test_modify_object_points(self):
+        grid_height = 6
+        grid_width = 9
+        grid_depth = 3
+        slice_of_object = 2
+        reshape_0 = -1
+        reshape_1 = 2
+
+        tester_object = np.zeros((grid_height*grid_width, grid_depth), np.float32)
+        modified_object = lf.modify_object_points(tester_object, grid_height,
+                                                  grid_width, slice_of_object,
+                                                  reshape_0, reshape_1)
+        self.assertListEqual(list(modified_object[-1]), [8., 5., 0.])
 
 
 if __name__ == '__main__':
