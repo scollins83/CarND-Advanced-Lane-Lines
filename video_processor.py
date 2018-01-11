9b8ccf56f440e01ce964b0accf4140e7e5811906
Sara Collins <datadivallc@gmail.com>
2018-01-10 21:59:46 -0600

    Added refactored video_processor file

--- video_processor.py
+++ video_processor.py
@@ -15,76 +15,12 @@ logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 
-def undistort_image(image, object_points, image_points):
-    """
-
-    :param image:
-    :param object_points:
-    :param image_points:
-    :return:
-    """
-    # Convert image to grayscale
-    gray_scale_image = lfc.convert_color_image_to_grayscale(image)
-
-    # Calibrate the camera
-    calibration_dict = lfc.calibrate_camera(object_points, image_points, gray_scale_image.shape[::-1])
-
-    # Return the undistorted image
-    return cv2.undistort(image, calibration_dict['camera_matrix'],
-                         calibration_dict['distortion_coeffs'],
-                         None,
-                         calibration_dict['camera_matrix'])
-
-
-def unwarp_corners(image, corners_width, corners_height, pattern_corners, camera_matrix, dist_coefficients):
-    """
-
-    :param image:
-    :param corners_width:
-    :param corners_height:
-    :param pattern_corners:
-    :param camera_matrix:
-    :param dist_coefficients:
-    :return:
-    """
-    undistorted_image = cv2.undistort(image, camera_matrix, dist_coefficients,
-                                      None, camera_matrix)
-
-    gray_undistorted = cv2.cvtColor()
-
-    if pattern_corners == "None":
-        input_corners = None
-    else:
-        input_corners = pattern_corners
-
-    ret, corners = cv2.findChessboardCorners(undistorted_image, (corners_width, corners_height), input_corners)
-
-    if ret == True:
-        offset = 100
-        image_size = (gray_undistorted.shape[1], gray_undistorted.shape[0])
-        source_points = np.float32([corners[0], corners[corners_width - 1],
-                                    corners[-1], corners[-corners_width]])
-        destination_points = np.float32([[offset, offset],
-                                         [image_size[0] - offset,
-                                         offset],
-                                        [image_size[0] - offset,
-                                         image_size[1] - offset],
-                                        [offset, image_size[1] - offset]])
-
-        matrix = cv2.getPerspectiveTransform(source_points, destination_points)
-        warped_image = cv2.warpPerspective(undistorted_image, matrix, image_size)
-
-        return warped_image, matrix
-
-
 def hls_select(img, thresh=(0, 255)):
     """
-    thresholds the S-channel of HLS
-    :param img:
-    :param thresh:
-    :return:
-
-    hls_binary = hls_select(image, thresh=(90, 255)
+    Thresholds the S-channel of HLS color space.
+    :param img: Image array.
+    :param thresh: Threshold tuple (min, max)
+    :return: Binary output.
     """
     hls = cv2.cvtColor(img, cv2.COLOR_RGB2HLS)
     s_channel = hls[:,:,2]
@@ -98,11 +34,11 @@ def abs_sobel_thresh(img, orient='x', thresh_min=0, thresh_max=255):
     """
     Takes an image, gradient orientation,
     # and threshold min / max values.
-    :param img:
-    :param orient:
-    :param thresh_min:
-    :param thresh_max:
-    :return:
+    :param img: Image array
+    :param orient: Orientation for sobel thresholding
+    :param thresh_min: Threshold minimum
+    :param thresh_max: Threshold maximum
+    :return: Binary image result
     """
     # Convert to grayscale
     gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
@@ -123,64 +59,13 @@ def abs_sobel_thresh(img, orient='x', thresh_min=0, thresh_max=255):
     return binary_output
 
 
-def mag_thresh(img, sobel_kernel=3, mag_thresh=(0, 255)):
-    """
-    Returns the magnitude of the gradient # for a given sobel kernel size and threshold values
-
-    :param img:
-    :param sobel_kernel:
-    :param mag_thresh:
-    :return:
-    """
-    # Convert to grayscale
-    gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
-    # Take both Sobel x and y gradients
-    sobelx = cv2.Sobel(gray, cv2.CV_64F, 1, 0, ksize=sobel_kernel)
-    sobely = cv2.Sobel(gray, cv2.CV_64F, 0, 1, ksize=sobel_kernel)
-    # Calculate the gradient magnitude
-    gradmag = np.sqrt(sobelx**2 + sobely**2)
-    # Rescale to 8 bit
-    scale_factor = np.max(gradmag)/255
-    gradmag = (gradmag/scale_factor).astype(np.uint8)
-    # Create a binary image of ones where threshold is met, zeros otherwise
-    binary_output = np.zeros_like(gradmag)
-    binary_output[(gradmag >= mag_thresh[0]) & (gradmag <= mag_thresh[1])] = 1
-
-    # Return the binary image
-    return binary_output
-
-
-
-def dir_threshold(img, sobel_kernel=3, thresh=(0, np.pi/2)):
-    """
-    Thresholds an image for a given range and Sobel kernel
-    :param img:
-    :param sobel_kernel:
-    :param thresh:
-    :return:
-    """
-    # Grayscale
-    gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
-    # Calculate the x and y gradients
-    sobelx = cv2.Sobel(gray, cv2.CV_64F, 1, 0, ksize=sobel_kernel)
-    sobely = cv2.Sobel(gray, cv2.CV_64F, 0, 1, ksize=sobel_kernel)
-    # Take the absolute value of the gradient direction,
-    # apply a threshold, and create a binary image result
-    absgraddir = np.arctan2(np.absolute(sobely), np.absolute(sobelx))
-    binary_output =  np.zeros_like(absgraddir)
-    binary_output[(absgraddir >= thresh[0]) & (absgraddir <= thresh[1])] = 1
-
-    # Return the binary image
-    return binary_output
-
-# Combination for HSV and HLS color_threshold
 def color_threshold(image, sthresh=(0, 255), vthresh=(0, 255)):
     """
-    Combines HSV and HLS
-    :param image:
-    :param sthresh:
-    :param vthresh:
-    :return:
+    Combination of the HSV and HLS colorspace thresholds
+    :param image: Image array
+    :param sthresh: Tuple threshold for the S-channel, (min, max)
+    :param vthresh: Tuble threshold for the V-channel, (min, max)
+    :return: Binary output
     """
     hls = cv2.cvtColor(image, cv2.COLOR_RGB2HLS)
     s_channel = hls[:, :, 2]
@@ -197,31 +82,32 @@ def color_threshold(image, sthresh=(0, 255), vthresh=(0, 255)):
     return output
 
 
-def window_mask(width, height, img_ref, center, level):
+def window_mask(width, height, img, center, level):
     """
-
-    :param width:
-    :param height:
-    :param img_ref:
-    :param center:
-    :param level:
-    :return:
+    Mask for drawing window areas for convolutional windows.
+    :param width: Window width
+    :param height: Window height
+    :param img: Image array
+    :param center: Window centroid level center
+    :param level: Window centroid level
+    :return: Masked window
     """
-    output = np.zeros_like(img_ref)
-    output[int(img_ref.shape[0]-(level+1)*height):int(img_ref.shape[0]-level*height), max(0, int(center-width)):min(int(center+width), img_ref.shape[1])] = 1
+    output = np.zeros_like(img)
+    output[int(img.shape[0] - (level + 1) * height):int(img.shape[0] - level * height),
+    max(0, int(center - width)):min(int(center + width), img.shape[1])] = 1
     return output
 
 
 def process_image(img):
     """
-
-    :param img:
-    :return:
+    Process the image for each video frame.
+    :param img: Image array.
+    :return: Processed image.
     """
     # Assert that at least a 3-channel image was passed to the function.
     assert img.shape[-1] == 3
 
-    # Undistort
+    # Undistort the image.
     img = cv2.undistort(img, matrix, dist, None, matrix)
 
     # Process image and generate binary pictures
@@ -231,24 +117,21 @@ def process_image(img):
     c_binary = color_threshold(img, sthresh=(config['color_s_thresh_min'], config['color_s_thresh_max']),
                                vthresh=(config['color_v_thresh_min'], config['color_v_thresh_max']))
     preprocessed_image[((gradx == 1) & (grady == 1) | (c_binary == 1))] = 255
-    # TODO: Lots of experimentation to how to get the best binary images
 
     # Set up Perspective Transform Area
     img_size = (img.shape[1], img.shape[0])
 
+    # Set up source and destination coordinates for transform.
     src = np.float32([[590, 460], [205, 720], [1127, 720], [695, 460]])
-
     dst = np.float32([[320, 0], [320, 720], [960, 720], [960, 0]])
 
     M = cv2.getPerspectiveTransform(src, dst)
     Minv = cv2.getPerspectiveTransform(dst, src)
     warped = cv2.warpPerspective(preprocessed_image, M, img_size, flags=cv2.INTER_LINEAR)
 
-    # Set up the overall class to do all the tracking
-    window_width = config['conv_window_width']
-    window_height = config['conv_window_height']
+    # Set up the overall Tracker class object to track lane line data
 
-    curve_centers = Tracker(window_width=window_width, window_height=window_height,
+    curve_centers = Tracker(window_width=config['conv_window_width'], window_height=config['conv_window_height'],
                             margin=config['tracker_margin'],
                             ym=config['tracker_ym_numerator'] / config['tracker_ym_denominator'],
                             xm=config['tracker_xm_numerator'] / config['tracker_xm_denominator'],
@@ -269,8 +152,10 @@ def process_image(img):
         leftx.append(window_centroids[level][0])
         rightx.append(window_centroids[level][1])
 
-        l_mask = window_mask(window_width, window_height, warped, window_centroids[level][0], level)
-        r_mask = window_mask(window_width, window_height, warped, window_centroids[level][1], level)
+        l_mask = window_mask(config['conv_window_width'], config['conv_window_height'],
+                             warped, window_centroids[level][0], level)
+        r_mask = window_mask(config['conv_window_width'], config['conv_window_height'],
+                             warped, window_centroids[level][1], level)
 
         # Add graphic points from window mask here to total pixels found
         l_points[(l_points == 255) | ((l_mask == 1))] = 255
@@ -287,7 +172,7 @@ def process_image(img):
 
     # Fit lane boundaries to left, right, center positions found
     yvals = range(0, warped.shape[0])
-    res_yvals = np.arange(warped.shape[0] - (window_height / 2), 0, -window_height)
+    res_yvals = np.arange(warped.shape[0] - (config['conv_window_height'] / 2), 0, - config['conv_window_height'])
 
     left_fit = np.polyfit(res_yvals, leftx, 2)
     left_fitx = left_fit[0] * yvals * yvals + left_fit[1] * yvals + left_fit[2]
@@ -298,13 +183,16 @@ def process_image(img):
     right_fitx = np.array(right_fitx, np.int32)
 
     left_lane = np.array(list(
-        zip(np.concatenate((left_fitx - window_width / 2, left_fitx[::-1] + window_width / 2), axis=0),
+        zip(np.concatenate((left_fitx - config['conv_window_width'] / 2,
+                            left_fitx[::-1] + config['conv_window_width'] / 2), axis=0),
             np.concatenate((yvals, yvals[::-1]), axis=0))), np.int32)
     right_lane = np.array(list(
-        zip(np.concatenate((right_fitx - window_width / 2, right_fitx[::-1] + window_width / 2), axis=0),
+        zip(np.concatenate((right_fitx - config['conv_window_width'] / 2,
+                            right_fitx[::-1] + config['conv_window_width'] / 2), axis=0),
             np.concatenate((yvals, yvals[::-1]), axis=0))), np.int32)
     middle_marker = np.array(list(
-        zip(np.concatenate((left_fitx + window_width / 2, right_fitx[::-1] - window_width / 2), axis=0),
+        zip(np.concatenate((left_fitx + config['conv_window_width'] / 2,
+                            right_fitx[::-1] - config['conv_window_width'] / 2), axis=0),
             np.concatenate((yvals, yvals[::-1]), axis=0))), np.int32)
 
     road = np.zeros_like(img)
@@ -324,7 +212,8 @@ def process_image(img):
     ym_per_pix = curve_centers.ym_per_pix
     xm_per_pix = curve_centers.xm_per_pix
 
-    curve_fit_cr = np.polyfit(np.array(res_yvals, np.float32) * ym_per_pix, np.array(leftx, np.float32) * xm_per_pix, 2)
+    curve_fit_cr = np.polyfit(np.array(res_yvals, np.float32) * ym_per_pix,
+                              np.array(leftx, np.float32) * xm_per_pix, 2)
     curverad = ((1 + (2 * curve_fit_cr[0] * yvals[-1] * ym_per_pix + curve_fit_cr[1]) ** 2) ** 1.5) / np.absolute(
         2 * curve_fit_cr[0])
 
